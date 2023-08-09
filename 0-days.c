ad53db359d44c8f898c0898e9ac76a81acaa91ba
Axis44 <kunenexolani34@gmail.com>
2023-08-09 07:20:12 -0700

    year days

--- 0-days.c
+++ 0-days.c
@@ -3,7 +3,7 @@
 
 /**
  * print_remaining_days - takes a date and prints number of days
- * left in the year, taking leap years into account
+ * 
  * @month: month in number format
  * @day: day of month
  * @year: year
